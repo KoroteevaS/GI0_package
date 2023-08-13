6d3e268db37829fc4fdd77331b6c4b3fec3170fc
Svetlana Koroteeva <Svetlana.Koroteeva@dia.govt.nz>
2023-08-13 19:51:14 +1200

    Update pagckage with more text

--- GI0/R/pGI0.R
+++ GI0/R/pGI0.R
@@ -1,11 +1,19 @@
-#' CDF
+#' Cumulative distribution function
 #'
+#' This function calculates the CDF from Fisher-Snedekor distribution
+#'
+#' @param x Numeric vector of input values at which to calculate the CDF.
+#' @param p_alpha Numeric, negative value controlling the roughness.
+#' @param p_gamma Numeric, positive value controlling the scale.
+#' @param p_Looks Numeric, positive value controlling the number of looks.
+#' @param log.p Logical; if TRUE, returns the log probability.
+#' @return Numeric vector of cumulative distribution values for the rGI0 distribution.
+#' @examples
 
-#' @param p_alpha Negative value that controls the roughness
-#' @param p_gamma Positive value that controls the scale
-#' @param p_Looks Positive value that controls the number of looks
-
-# cumulative distribution function
-pGI0 <- function(x, p_alpha, p_gamma, p_Looks, log.p=FALSE) {
-  return(pf(-p_alpha * x / p_gamma, df1 = 2*p_Looks, df2=-2*p_alpha, log.p))
+pGI0 <- function(x, p_alpha, p_gamma, p_Looks, log.p = FALSE) {
+  if (p_alpha < 0 & p_gamma > 0 & p_Looks >= 1) {
+    return(pf(-p_alpha * x / p_gamma, df1 = 2 * p_Looks, df2 = -2 * p_alpha, log.p))
+  } else {
+    stop("Invalid parameters. The conditions are not met.")
+  }
 }
--- GI0/R/qGI0.R
+++ GI0/R/qGI0.R
@@ -1,11 +1,16 @@
 #' Quantile function
 #'
+#' @param p Probability values for which to calculate the quantiles
 #' @param p_alpha Negative value that controls the roughness
 #' @param p_gamma Positive value that controls the scale
 #' @param p_Looks Positive value that controls the number of looks
-#' @param lower.tatil Optional parameter that computes only the lower tail
-#' @param log optional parameter that switches logarithm
-# quantile function
+#' @param lower.tail Logical; if TRUE, computes lower tail quantiles, else upper tail quantiles
+#' @param log.p Logical; if TRUE, input probabilities are given as log probabilities
+#' @return The quantile values for the rGI0 distribution
 qGI0 <- function(p, p_alpha, p_gamma, p_Looks, lower.tail = TRUE, log.p = FALSE) {
-  return(qf(p, df1=2*p_Looks, df2=-2*p_alpha, lower.tail, log.p))
+  if (p_alpha < 0 & p_gamma > 0 & p_Looks >= 1) {
+      return(qf(p, df1 = 2 * p_Looks, df2 = -2 * p_alpha, lower.tail, log.p))
+  } else {
+    stop("Invalid parameters. The conditions are not met.")
+  }
 }
